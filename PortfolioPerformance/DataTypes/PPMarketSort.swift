966871d7d3fa0a0263856898d5ee641b119d8c41
Sandor Suszta <sasha.shusta@gmail.com>
2023-04-17 18:07:39 +0100

    Add MarketSort enum

---
+++ PortfolioPerformance/DataTypes/PPMarketSort.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum PPMarketSort: String {
+    case topCaps = "Highest Cap"
+    case topWinners = "Top Winners"
+    case topLosers = "Top Losers"
+    case topVolumes = "Top Volume"
+}
