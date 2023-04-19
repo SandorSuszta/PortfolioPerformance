3d96c59452a1ce40c90fd62a5ee18d0ba53accf5
Sandor Suszta <sasha.shusta@gmail.com>
2023-04-19 19:53:18 +0100

    Add delete row functionality to Watchlist

--- PortfolioPerformance/Scenes/Watchlist/WatchlistDataSource.swift
+++ PortfolioPerformance/Scenes/Watchlist/WatchlistDataSource.swift
@@ -12,7 +12,13 @@ final class WatchlistDataSource: UITableViewDiffableDataSource<WatchlistSection,
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-           
+            var snapshot = self.snapshot()
+            
+            if let item = itemIdentifier(for: indexPath) {
+                snapshot.deleteItems([item])
+                UserDefaultsService.shared.deleteFrom(.watchlist, ID: item.id)
+                apply(snapshot, animatingDifferences: true)
+            }
         }
     }
 }
