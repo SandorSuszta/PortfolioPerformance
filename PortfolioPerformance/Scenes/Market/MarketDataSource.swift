26b3c579a54f4c7a52bc52d579adca30a4e61318
Sandor Suszta <sasha.shusta@gmail.com>
2023-06-21 12:02:57 +0100

    Add documentation to MarketDataSource

--- PortfolioPerformance/Scenes/Market/MarketDataSource.swift
+++ PortfolioPerformance/Scenes/Market/MarketDataSource.swift
@@ -1,11 +1,17 @@
 import UIKit
 
+/// A custom data source class for a UICollectionView. CollectionView has two different sections each with different cells and view models.
 final class MarketDataSource: UICollectionViewDiffableDataSource<MarketSection, MarketItemViewModel> {
     
     //MARK: - Init
     
+    /// Initializer handles the cell configuration logic based on the type of the item. It differentiates between two types of items: "Market Cards" and "CryptoCoins".
+    ///
+    /// For "Market Cards", it further distinguishes between different cell types based on the cellType property of the associated view model.
+    /// It creates the corresponding cell instances (MarketCardMetricCell or MarketCardGreedAndFearCell), configures them with the associated view model, and returns the configured cell.
     init(collectionView: UICollectionView) {
         super.init(collectionView: collectionView) { collectionView, indexPath, item in
+           
             switch item {
                 
                 //Market Cards
