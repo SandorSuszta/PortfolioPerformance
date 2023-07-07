14212b49a6db4b293c2fcbf1b3c0cb034960080b
Sandor Suszta <sasha.shusta@gmail.com>
2023-07-07 20:42:44 +0100

    Create DetailsCell enum

--- PortfolioPerformance/Common/Views/Custom Cells/DetailsTableCell/DetailsCellVM.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-struct DetailsCellsViewModel {
-    public var name: String
-    public var value: String
-}
---
+++ PortfolioPerformance/Scenes/CoinDetails/DataTypes/DetailCell.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum DetailCell: String {
+    case marketCap =            "Market Cap Value"
+    case volume =               "Volume"
+    case circulatingSupply =    "Circulating supply"
+    case totalSupply =          "Total supply"
+    case maxSupply =            "Max supply"
+    case allTimeHigh =          "All time high"
+    case changeFromATH =        "Change percentage from ATH"
+    case ATHDate =              "ATH date"
+}
