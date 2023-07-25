fd21b0d1287b7b7ade2684f9156624a99f5b98f9
Sandor Suszta <sasha.shusta@gmail.com>
2023-07-25 22:45:48 +0100

    Delete MarketItem hashable conformance

--- PortfolioPerformance/Scenes/Market/DataTypes/MarketItem.swift
+++ PortfolioPerformance/Scenes/Market/DataTypes/MarketItem.swift
@@ -6,38 +6,3 @@ enum MarketItem: Hashable {
     case marketCard(MarketCard)
     case cryptoCoinCell(CryptoCoinCell)
 }
-
-///Hashable conformance is required by DiffableDataSource
-//extension MarketItem: Hashable {
-//    func hash(into hasher: inout Hasher) {
-//        switch self {
-//
-//        case .marketCard(let card):
-//            switch card {
-//            case .dataReceived(let model):
-//                hasher.combine(model)
-//            case .loading(let index):
-//                hasher.combine(index)
-//            }
-//
-//        case .cryptoCoinCell(let cell):
-//            switch cell {
-//            case .dataReceived(let model):
-//                hasher.combine(model)
-//            case .loading(let index):
-//                hasher.combine(index)
-//            }
-//        }
-//    }
-    
-//    static func ==(lhs: MarketItem, rhs: MarketItem) -> Bool {
-//        switch (lhs, rhs) {
-//        case (.marketCard(let lhsCard), .marketCard(let rhsCard)):
-//            return lhsCard == rhsCard
-//        case (.cryptoCoinCell(let lhsViewModel), .cryptoCoinCell(let rhsViewModel)):
-//            return lhsViewModel == rhsViewModel
-//        default:
-//            return false
-//        }
-//    }
-//}
