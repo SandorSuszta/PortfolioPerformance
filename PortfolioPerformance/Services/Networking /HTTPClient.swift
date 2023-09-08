45b47c97a6a16968883e3ca6f5cc0aacdc2dbe76
Sandor Suszta <sasha.shusta@gmail.com>
2023-09-08 21:59:37 +0100

    Create HTTPClient

--- PortfolioPerformance/Services/Networking /HTTPClient.swift
+++ PortfolioPerformance/Services/Networking /HTTPClient.swift
@@ -1,32 +1,5 @@
 import Foundation
 
 protocol HTTPClient {
-    func performRequest<T: APIRequest>(_ request: T) async throws -> T.ResponseType
+    func performRequest<T: Decodable>(url: URL?, responseModel: T.Type, completionHandler: @escaping (Result<T, Error>) -> Void)
 }
-
-protocol APIRequest {
-    associatedtype ResponseType: Decodable
-    
-    var endpoint: Endpoint { get }
-}
-
-protocol Endpoint {
-    var scheme: String { get }
-    var host: String { get }
-    var path: String { get }
-    var parameters: [URLQueryItem] { get }
-    var url: URL? { get }
-}
-
-extension Endpoint {
-    var url: URL? {
-        var components = URLComponents()
-        components.scheme = scheme
-        components.host = host
-        components.path = path
-        components.queryItems = parameters
-        
-        return components.url
-    }
-}
-
